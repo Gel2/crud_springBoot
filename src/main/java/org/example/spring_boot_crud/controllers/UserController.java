152b40feb5fdcb32dd5ccac332d8b94a3fcdd36b
Gel2 <goncharovceo@mail.ru>
2024-02-28 23:59:46 +1000

    second commit

--- src/main/java/org/example/spring_boot_crud/controllers/UserController.java
+++ src/main/java/org/example/spring_boot_crud/controllers/UserController.java
@@ -2,14 +2,20 @@ package org.example.spring_boot_crud.controllers;
 
 import org.example.spring_boot_crud.models.User;
 import org.example.spring_boot_crud.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/users")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -18,46 +24,58 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping()
+    @GetMapping("/")
+    public String redirectToUsers() {
+        log.info("Перенаправление на /users");
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
     public String getAllUsers(Model model) {
+        log.info("Получение всех пользователей");
         model.addAttribute("users", userService.getAllUsers());
         return "users/index";
     }
 
-    @GetMapping("/show")
+    @GetMapping("/users/show")
     public String getUser(@RequestParam("id") int id, Model model) {
+        log.info("Получение пользователя с id: {}", id);
         model.addAttribute("user", userService.getUser(id));
         return "users/show";
     }
 
-    @GetMapping("/new")
+    @GetMapping("/users/new")
     public String newUser(Model model) {
+        log.info("Создание нового пользователя");
         model.addAttribute("user", new User());
         return "users/new";
     }
 
-    @PostMapping
+    @PostMapping("/users")
     public String addUser(@ModelAttribute("user") User user) {
+        log.info("Добавление пользователя: {}", user);
         userService.addUser(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/edit")
+    @GetMapping("/users/edit")
     public String editUser(@RequestParam("id") int id, Model model) {
+        log.info("Редактирование пользователя с id: {}", id);
         model.addAttribute("user", userService.getUser(id));
         return "users/edit";
     }
 
-    @PostMapping("/update")
+    @PostMapping("/users/update")
     public String updateUser(@ModelAttribute("user") User user, @RequestParam("id") int id) {
+        log.info("Обновление пользователя с id: {}", id);
         userService.updateUser(id, user);
         return "redirect:/users";
     }
 
-    @PostMapping("/delete")
+    @PostMapping("/users/delete")
     public String deleteUser(@RequestParam("id") int id) {
+        log.info("Удаление пользователя с id: {}", id);
         userService.deleteUser(id);
         return "redirect:/users";
     }
-
 }
--- src/main/java/org/example/spring_boot_crud/models/User.java
+++ src/main/java/org/example/spring_boot_crud/models/User.java
@@ -1,26 +1,37 @@
 package org.example.spring_boot_crud.models;
 
 import jakarta.persistence.*;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
 
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    @Size(min = 2, max = 50, message = "Имя должно содержать от 2 до 50 символов")
     @Column(name = "name")
     private String name;
+
+    @Min(value = 1, message = "Возраст должен быть не меньше 1")
+    @Max(value = 120, message = "Возраст должен быть не больше 120")
     @Column(name = "age")
     private int age;
 
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
     @Column(name = "email")
     private String email;
 
-
     public User() {
     }
-
 
     public User(String name, int age, String email) {
         this.name = name;
@@ -34,6 +45,16 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 
     public String getName() {
@@ -59,6 +80,4 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
-
 }
